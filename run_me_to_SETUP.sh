115fa20e93fa85bfbf69d732d4649cc6e517aff5
admin <admin@local>
2025-07-07 14:45:20 +0700

    fix

--- run_me_to_SETUP.sh
+++ run_me_to_SETUP.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p $HOME/.config
-mkdir -p $HOME/.local/{share, fonts}
+mkdir -p $HOME/.local/share
+mkdir -p $HOME/.local/fonts
 
 # bashrc
 echo "source $HOME/.mybashrc" >> $HOME/.bashrc
