95d532e1c585cf8ae5e40ca0a948b0e9f770d8fe
admin <admin@local>
2026-02-05 12:11:40 +0700

    update

--- homedir/scripts/install__lf-config.sh
+++ homedir/scripts/install__lf-config.sh
@@ -4,9 +4,9 @@ echo "install script: lf & lf-config"
 # https://codeberg.org/tplasdio/lf-config
 # pinned-commit: 6104f8063b8d6c9f7cc15a5e0e459e67754713e7
 
-sudo apt install lf
-(command -v apt > /dev/nul 2>&1) && (sudo apt install lf)
-(command -v pacman > /dev/nul 2>&1) && (sudo pacman -Syu lf)
+# sudo apt install lf
+(command -v apt > /dev/null 2>&1) && (sudo apt install lf)
+(command -v pacman > /dev/null 2>&1) && (sudo pacman -Syu lf)
 
 mkdir -p ~/.config
 git clone https://codeberg.org/tplasdio/lf-config ~/.config/lf-config
